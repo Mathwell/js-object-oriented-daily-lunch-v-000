2cffcbfb0a143cc8c5f4f9bab3599a3ffbb84710
Olena Ageyeva <eageeva@hotmail.com>
2018-06-02 23:04:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,9 +42,12 @@ class Customer{
   }
 
   deliveries(){
-    return store.deliveries.filter(delivery =>{
-      delivery.customerId===this.id;
-    })
+    //return store.deliveries.filter(delivery =>{
+    //  delivery.customerId===this.id;
+    //})
+    return store.deliveries.filter(delivery => {
+        return delivery.customerId == this.id;
+      });
   }
 
 }
