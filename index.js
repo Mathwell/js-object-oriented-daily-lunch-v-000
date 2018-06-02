f9c78e75a60243cbe099f6e8edb3b226be4f344a
Olena Ageyeva <eageeva@hotmail.com>
2018-06-02 22:22:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId=0;
 
 class Neighborhood{
   constructor(name){
     this.name=name;
+    this.id=neighborhoodId++;
   }
 }
 
