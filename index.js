14329415150389388fa74bdbfccc3d24f893f35d
Olena Ageyeva <eageeva@hotmail.com>
2018-06-03 00:11:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,6 +78,12 @@ class Customer{
     })
   }
 
+  totalSpent(){
+    prices=this.meals().map(meal =>{
+    return meal.price})
+    return prices.reduce(function(sum,value){return sum+value})
+  }
+
 
 }
 
